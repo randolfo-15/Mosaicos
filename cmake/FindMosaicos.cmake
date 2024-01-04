05476298785924484b6bd9c519d92facef5a9587
Randolfo <ragnifico@yahoo.com.br>
2024-01-04 12:41:14 -0300

    Versao 04/01/24

--- cmake/FindMosaicos.cmake
+++ cmake/FindMosaicos.cmake
@@ -2,10 +2,16 @@ include(FindPackageHandleStandardArgs)
 
 find_path(MOSAICOS_INCLUDE_DIRS 
         NAMES 
-                mosaicos/color.hpp
-                mosaicos/tema.hpp
-                mosaicos/printables.hpp
-                mosaicos/display.hpp 
+                mosaicos/str_rag.hpp
+                mosaicos/color_rag.hpp
+                mosaicos/effect_rag.hpp
+                mosaicos/ground_rag.hpp
+                mosaicos/background_rag.hpp 
+                mosaicos/foreground_rag.hpp 
+                mosaicos/theme_rag.hpp 
+                mosaicos/directions_rag.hpp
+                mosaicos/clear_rag.hpp
+                mosaicos/display_rag.hpp 
         HINTS 
                 "${CMAKE_SOURCE_DIR}/project")
 
